836731896250c73d7047ed78490fb79766384378
kyunggwan <4562sky@naver.com>
2024-04-17 17:47:40 +0900

    test: @ComponentScan 속성 테스트

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 이 어노테이션에 기본적으로 @ComponentScan이 있음
 public class CoreApplication {
 
     public static void main(String[] args) {
--- src/test/java/hello/core/AutoAppConfig.java
+++ src/test/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,13 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) // @Component가 있는 모든 class를 빈으로 등록, Configuration어노테이션 붙은 class는 제외
+// basePackages는 경로 시작 위치
+// basePackageClasses는 지정한 클래스의 패키지를 탐색 시작 위치로 지정
+// 지정하지 않으면 현재 경로 (hello.core) 부터 탐색 시작
 public class AutoAppConfig {
 
 }
