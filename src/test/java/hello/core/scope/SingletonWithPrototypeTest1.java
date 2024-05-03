ebd70e0b4b3de462f79cef9c5fadc67290fff51e
kyunggwan <4562sky@naver.com>
2024-05-03 13:24:17 +0900

    test: 싱글톤 빈과 프로토타입을 같이 썼을 때 Provider로 해결법 테스트 - 테스트 오류

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import lombok.RequiredArgsConstructor;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -18,7 +20,9 @@ import static org.assertj.core.api.Assertions.*;
 * 프로토타입 빈이 새로 생성되기는 하지만, 싱글톤 빈과 계속 유지되는 것은 문제점
 * 프로토타입 빈은 사용할때마다 새로 생성되어야 함
 * */
-public class SingletonWiehPrototypeTest1 {
+
+@ComponentScan
+public class SingletonWithPrototypeTest1 {
 
     @Test
     void prototypeFind() {
@@ -35,6 +39,7 @@ public class SingletonWiehPrototypeTest1 {
 
     @Test
     void singletonClientUserPrototype(){
+        // 스프링 컨텍스트를 생성하고 빈을 가져옴
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
 
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
@@ -44,16 +49,20 @@ public class SingletonWiehPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
         // 같은 count가 증가함
     }
 
     @Scope("singleton")
-    @RequiredArgsConstructor
+    @Component
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
+
+        // ObjectProvider를 통해 싱글톤 빈과 프로토타입을 함께 사용가능
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
@@ -61,6 +70,7 @@ public class SingletonWiehPrototypeTest1 {
     }
 
     @Scope("prototype")
+    @Component
     static class PrototypeBean {
         private int count = 0;
 
